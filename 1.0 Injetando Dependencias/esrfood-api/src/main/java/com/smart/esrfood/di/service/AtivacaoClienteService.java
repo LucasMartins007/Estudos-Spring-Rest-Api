e41f9a7a99870d4d9d79d46c896ff03bc7196cee
LucasMartins007 <73621789+LucasMartins007@users.noreply.github.com>
2021-04-07 15:27:24 -0300

    Injeção de Dependencias

--- 1.0 Injetando Dependencias/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
+++ 1.0 Injetando Dependencias/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
@@ -5,6 +5,15 @@ import org.springframework.stereotype.Component;
 import com.smart.esrfood.di.model.Cliente;
 import com.smart.esrfood.di.notificador.Notificador;
 
+// A injeção de dependencias do Spring permite que façamos a 
+// implementação somente da interface, e o Spring irá reconhecer
+// qual classe deverá implementar.
+
+// nesse caso, temos a classe NotificadorEmail, 
+// que esta implementa a interface Notificador,
+// nesse caso, podemos implementar somente a interface, 
+// que o Spring irá reconhecer que deve instancia a classe NotificadorEmail tambem.
+
 @Component
 public class AtivacaoClienteService {
 

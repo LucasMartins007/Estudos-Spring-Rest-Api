da54b9a566e8e343298a3b36df6b934482147882
LucasMartins007 <73621789+LucasMartins007@users.noreply.github.com>
2021-04-07 14:01:05 -0300

    Update AtivacaoClienteService.java

--- 1.4 Injeção de Lista Beans/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
+++ 1.4 Injeção de Lista Beans/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 
 import com.smart.esrfood.di.model.Cliente;
 import com.smart.esrfood.di.notificador.Notificador;
-
+// Nessa caso temos duas classes que implementam a interface Notificador, a NotificadorEmail e a NotificadorSMS.
+// Para evitar que o Spring tente injetar duas classes em um instacia, estourando assim um erro.. 
+// .. podemos criar uma lista de notificadores para que possamos injetar ambas as classes , ou mais caso necessário.
 
 @Component
 public class AtivacaoClienteService {

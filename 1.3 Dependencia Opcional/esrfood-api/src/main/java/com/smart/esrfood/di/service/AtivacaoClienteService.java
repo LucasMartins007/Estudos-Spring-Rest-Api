e7dfd79706230f8ec6c4d5fe21da19845e3371a3
LucasMartins007 <73621789+LucasMartins007@users.noreply.github.com>
2021-04-07 14:03:48 -0300

    Update AtivacaoClienteService.java

--- 1.3 Dependencia Opcional/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
+++ 1.3 Dependencia Opcional/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
@@ -6,18 +6,19 @@ import org.springframework.stereotype.Component;
 import com.smart.esrfood.di.model.Cliente;
 import com.smart.esrfood.di.notificador.Notificador;
 
-	// Quando adicionamos somente o @Autowired, o spring interpreta que essa instância é obrigatória para a..
-	// .. classe. 
-	// nesse caso, se o notificador viesse nulo, daria um erro de compilação.
+// Quando definimos somente o @Autowired, 
+//o spring interpreta que essa instância é obrigatória para a classe,
+//nesse caso, se o notificador viesse nulo, daria um erro de compilação.
+
+// Quando adicionamos o required, 
+// definimos se essa instância é obrigatoria para a classe OU NÃO.
+// nesse caso, tratamos mais abaixo que se caso o notificador for nullo,
+// o sistema irá tratar isso e exibir uma mensagem melhor trabalhada para o usuário
 	
-	// Quando adicionamos o required, definimos se essa instância é obrigatoria para a classe ou não.
-	
-	// nesse caso, tratamos mais abaixo caso o notificador for nullo para exibir uma mensagem melhor..
-	// ..trabalhada para o usuário
-	
-	// na prática, se o @Component do notificadorEmail esta comentado, ou seja, ele irá ser..
-	// ..instanciado nessa classe com valor nulo, e ao invés de estourar um erro de compilação..
-	// ..ele trata o erro, e informa que não existe o notificador.
+// na prática, se o @Component do notificadorEmail esta comentado, ou seja, ele irá ser..
+// ..instanciado nessa classe com valor nulo, e ao invés de estourar um erro de compilação..
+// ..ele trata o erro, e informa que não existe o notificador.
+
 @Component
 public class AtivacaoClienteService {
 

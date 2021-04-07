de16a9a11420a135ac292cb718455a9b2e4e9263
LucasMartins007 <73621789+LucasMartins007@users.noreply.github.com>
2021-04-07 10:53:44 -0300

    Curso Spring Rest API - 2.12

---
+++ 1.0 Injetando Dependencias/esrfood-api/src/main/java/com/smart/esrfood/di/notificador/NotificadorEmail.java
@@ -0,0 +1,20 @@
+package com.smart.esrfood.di.notificador;
+
+import org.springframework.stereotype.Component;
+
+import com.smart.esrfood.di.model.Cliente;
+
+@Component
+public class NotificadorEmail implements Notificador {
+	
+	public NotificadorEmail() {
+		System.out.println("NotificadorEmail");
+	}
+	
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+	
+}
---
+++ 1.0 Injetando Dependencias/esrfood-api/src/main/java/com/smart/esrfood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,23 @@
+package com.smart.esrfood.di.service;
+
+import org.springframework.stereotype.Component;
+
+import com.smart.esrfood.di.model.Cliente;
+import com.smart.esrfood.di.notificador.Notificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	private Notificador notificador;
+	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		System.out.println("AtivacaoClienteService: " + notificador);
+	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+	
+}
